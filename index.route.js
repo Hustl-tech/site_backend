2f7ab4c373c93cebd51961eb3967f5bdd2bdb3a2
Prabhav <prabhavlamichhane77@gmail.com>
2022-01-22 18:24:07 +0545

    init and auth added

--- index.route.js
+++ index.route.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const userRoutes = require('./server/user/user.route');
 const authRoutes = require('./server/auth/auth.route');
 
 const router = express.Router(); // eslint-disable-line new-cap
@@ -8,11 +7,8 @@ const router = express.Router(); // eslint-disable-line new-cap
 
 /** GET /health-check - Check service health */
 router.get('/health-check', (req, res) =>
-  res.send('OK')
+    res.send('OK')
 );
-
-// mount user routes at /users
-router.use('/users', userRoutes);
 
 // mount auth routes at /auth
 router.use('/auth', authRoutes);
